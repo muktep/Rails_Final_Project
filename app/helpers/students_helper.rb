a493874b09a754feddc000ae56991521cae06912
Mukte <pratik.mukte@dxc.com>
2020-12-03 00:58:11 +0530

    adding files

---
+++ app/helpers/students_helper.rb
@@ -0,0 +1,14 @@
+module StudentsHelper
+    def name(student)
+        if student.gender.downcase == "m" or student.gender.downcase == "male"
+         "Mr."+" "+student.first_name+" "+student.last_name
+        else
+            "Mrs."+" "+student.first_name+" "+student.last_name
+        end
+    end
+    def age(student)
+        time=Time.now
+        age = time.year-student.year_of_birth
+        return age
+    end
+end
